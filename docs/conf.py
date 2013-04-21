e892d30675117996608c896014a61bf57c435987
@damnwidget <oscar.campos@member.fsf.org>
2013-04-21 21:48:06 +0100

    some options commented on conf.py for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+# html_theme = 'bootstrap'
 
 html_short_title = 'Mamba'
 
@@ -126,7 +128,7 @@ html_short_title = 'Mamba'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
