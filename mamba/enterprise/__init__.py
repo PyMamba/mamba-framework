b59e0c678ee4a387c9b612149194952be07f0250
@damnwidget <oscar.campos@member.fsf.org>
2013-11-06 19:42:58 +0000

    added all the storm imports to mamba.enterprise scope so we can now do "from mamba.enterprise import Int, Unicode, Bool, NativeEnum, Reference"

--- mamba/enterprise/__init__.py
+++ mamba/enterprise/__init__.py
@@ -2,11 +2,34 @@
 # Copyright (c) 2012 Oscar Campos <oscar.campos@member.fsf.org>
 # See LICENSE for more details
 
-__doc__ = '''
+"""
 Subpackage containing the modules that implement database abstraction layer
-'''
+"""
 
-from .database import Database
+from storm.properties import List, Decimal, DateTime, Date, Time, Enum, UUID
+from storm.expr import Like, In, Asc, Desc, And, Or, Min, Max, Count, Not
+from storm.properties import Bool, Int, Float, RawStr, Chars, Unicode
+from storm.expr import Select, Insert, Update, Delete, Join, SQL
+from storm.references import Reference, ReferenceSet, Proxy
+from storm.properties import TimeDelta, Pickle, JSON
+from storm.store import Store, AutoReload
+from storm.info import ClassAlias
+from storm.base import Storm
+
+from .common import NativeEnum
+from .database import Database, transact
 
 
-__all__ = ['Database']
+__all__ = [
+    'Database', 'transact',
+    'NativeEnum',
+    'Bool', 'Int', 'Float', 'RawStr', 'Chars', 'Unicode',
+    'List', 'Decimal', 'DateTime', 'Date', 'Time', 'Enum', 'UUID',
+    'TimeDelta', 'Pickle', 'JSON',
+    'Reference', 'ReferenceSet', 'Proxy',
+    'Store', 'AutoReload',
+    'Select', 'Insert', 'Update', 'Delete', 'Join', 'SQL',
+    'Like', 'In', 'Asc', 'Desc', 'And', 'Or', 'Min', 'Max', 'Count', 'Not',
+    'ClassAlias',
+    'Storm'
+]
