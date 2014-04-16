344492c93b39d84a10948429605e9cf0f7ce60c0
Nicholas Amorim <nicholas.santos@dedsert.com>
2014-04-16 15:51:58 +0100

    Added ujson support.

--- mamba/utils/__init__.py
+++ mamba/utils/__init__.py
@@ -2,6 +2,14 @@
 # Copyright (c) 2012 Oscar Campos <oscar.campos@member.fsf.org>
 # See LICENSE for more details
 
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+__all__ = ['json']
+
 __doc__ = '''
 Subpackage containing the modules that implement common utilities
 '''
